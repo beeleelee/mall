4e818c178c0501dd461198fe8b93c253cfe0151b
beeleelee <beeleelee@outlook.com>
2026-07-04 09:28:05 +0800

    Add migration 000015 for wallet_provider + wallet_token columns

---
+++ infrastructure/database/migrations/000015_add_checkout_payment_token.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE checkout_sessions ADD COLUMN IF NOT EXISTS wallet_provider TEXT NOT NULL DEFAULT '';
+ALTER TABLE checkout_sessions ADD COLUMN IF NOT EXISTS wallet_token TEXT NOT NULL DEFAULT '';
